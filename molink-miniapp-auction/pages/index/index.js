1ed0489edcbb0282c67195e6d9c35d529c524441
baimojushi <2636520057@qq.com>
2026-04-05 22:53:43 +0800

    增加首页选项交互动画

--- molink-miniapp-auction/pages/index/index.js
+++ molink-miniapp-auction/pages/index/index.js
@@ -1,22 +1,34 @@
 const app = getApp()
 const { request } = require('../../utils/helper')
 
+const INTRO_DURATION = 2600
+const HANG_DURATION = 2200
+const RECOMMEND_BRIDGE_DURATION = 2200
+
+const ANIMATION_ASSETS = {
+  intro: '/assets/icons/1.webp',
+  hang: '/assets/icons/2.1.webp',
+  recommendBridge: '/assets/icons/2.webp',
+  recommendLoop: '/assets/icons/2.2.webp'
+}
+
 Page({
+  animationTimer: null,
+  currentStep: '',
+  desiredService: '',
   data: {
     services: [
       {
         id: 'hang_in_home',
         title: '作品挂进家',
         desc: '把画作挂置于您居住的空间中，生成真实效果图',
-        iconPath: '/assets/icons/huakuang.svg',
-        animationKey: 'hang'
+        iconPath: '/assets/icons/huakuang.svg'
       },
       {
         id: 'recommend_work',
         title: '为空间推荐作品',
         desc: '根据您的居住空间，为您精选合适的艺术作品',
-        iconPath: '/assets/icons/xiaofangzi.svg',
-        animationKey: 'recommend'
+        iconPath: '/assets/icons/xiaofangzi.svg'
       }
     ],
     selectedService: null,
@@ -24,18 +36,125 @@ Page({
     showLoginOverlay: false,
     loginLoading: false,
     historyCount: 0,
-    selectedAnimationKey: ''
+    animationSrc: ANIMATION_ASSETS.intro,
+    animationVisible: true,
+    animationLoop: false
   },
 
   onLoad() {
     if (!app.globalData.openid) {
       this.setData({ showLoginOverlay: true })
     }
+    this.startAnimationStep('intro')
   },
 
   onShow() {
     this.checkActiveOrder()
     this.loadHistoryCount()
+  },
+
+  onUnload() {
+    this.clearAnimationTimer()
+  },
+
+  clearAnimationTimer() {
+    if (this.animationTimer) {
+      clearTimeout(this.animationTimer)
+      this.animationTimer = null
+    }
+  },
+
+  startAnimationStep(stepName) {
+    this.clearAnimationTimer()
+    this.currentStep = stepName
+
+    const config = {
+      intro: { src: ANIMATION_ASSETS.intro, loop: false, duration: INTRO_DURATION },
+      hang: { src: ANIMATION_ASSETS.hang, loop: false, duration: HANG_DURATION },
+      recommendBridge: { src: ANIMATION_ASSETS.recommendBridge, loop: false, duration: RECOMMEND_BRIDGE_DURATION },
+      recommendLoop: { src: ANIMATION_ASSETS.recommendLoop, loop: true, duration: 0 }
+    }[stepName]
+
+    if (!config) return
+
+    this.setData({
+      animationVisible: false
+    })
+
+    setTimeout(() => {
+      this.setData({
+        animationSrc: config.src,
+        animationLoop: config.loop,
+        animationVisible: true
+      })
+    }, 20)
+
+    if (config.loop) return
+
+    this.animationTimer = setTimeout(() => {
+      this.animationTimer = null
+      this.resolveAnimationStep(stepName)
+    }, config.duration)
+  },
+
+  resolveAnimationStep(stepName) {
+    if (stepName === 'intro') {
+      if (this.desiredService === 'hang_in_home') {
+        this.startAnimationStep('hang')
+        return
+      }
+      if (this.desiredService === 'recommend_work') {
+        this.startAnimationStep('recommendBridge')
+        return
+      }
+      this.currentStep = 'introDone'
+      return
+    }
+
+    if (stepName === 'hang') {
+      if (this.desiredService === 'recommend_work') {
+        this.startAnimationStep('recommendLoop')
+        return
+      }
+      this.currentStep = 'hangDone'
+      return
+    }
+
+    if (stepName === 'recommendBridge') {
+      if (this.desiredService === 'hang_in_home') {
+        this.startAnimationStep('hang')
+        return
+      }
+      this.startAnimationStep('recommendLoop')
+    }
+  },
+
+  syncAnimationWithSelection(serviceId) {
+    this.desiredService = serviceId
+
+    if (!serviceId) return
+
+    if (serviceId === 'hang_in_home') {
+      if (this.currentStep === 'recommendLoop') {
+        this.startAnimationStep('hang')
+        return
+      }
+      if (['intro', 'hang', 'recommendBridge'].includes(this.currentStep)) {
+        return
+      }
+      this.startAnimationStep('hang')
+      return
+    }
+
+    if (serviceId === 'recommend_work') {
+      if (this.currentStep === 'recommendLoop') return
+      if (['intro', 'hang', 'recommendBridge'].includes(this.currentStep)) return
+      if (this.currentStep === 'hangDone') {
+        this.startAnimationStep('recommendLoop')
+        return
+      }
+      this.startAnimationStep('recommendBridge')
+    }
   },
 
   async doWxLogin() {
@@ -100,11 +219,8 @@ Page({
 
   selectService(e) {
     const selectedService = e.currentTarget.dataset.id
-    const matched = this.data.services.find(item => item.id === selectedService)
-    this.setData({
-      selectedService,
-      selectedAnimationKey: matched ? matched.animationKey : ''
-    })
+    this.setData({ selectedService })
+    this.syncAnimationWithSelection(selectedService)
   },
 
   goNext() {
